3ed65f55025b8f6f398b4ae4d3500afdb30d519e
RyFry <ryanlorey@outlook.com>
2016-04-21 21:11:31 -0500

    Reverting an old commit because I realized unique_ptr is not what we want

---
+++ html/search/typedefs_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['scenegraphptr',['SceneGraphPtr',['../namespaceLarp.html#a261f2d9b2a675a22412db9ab343b1ee0',1,'Larp']]]
+];
---
+++ html/search/typedefs_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['uniqueentity',['UniqueEntity',['../namespaceLarp.html#ad6d203c6dc3d8ea7a5517a64e1665403',1,'Larp']]],
+  ['uniquenode',['UniqueNode',['../namespaceLarp.html#ad95a88bc34f8c78cefd64c9bbeb94a58',1,'Larp']]],
+  ['uniquescenegraph',['UniqueSceneGraph',['../namespaceLarp.html#a81a0d129ec1fc8f1f9fa231fbba6b19b',1,'Larp']]]
+];
