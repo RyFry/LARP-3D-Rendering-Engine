30a5d45ce7a07afc01028472302e7bb5b36ecd89
Phoenix Bishea <phoenixb@utexas.edu>
2016-04-26 20:39:46 -0500

    Update Entity draw() docs

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -30,7 +30,6 @@ namespace Larp
         // Directional light
         if (use_directional_lighting)
         {
-          // std::cout << "dir light" << std::endl;
           glUniform3f(glGetUniformLocation(this->_shader._program, "dirLight.direction"), directional_lights[0]->_direction.x, directional_lights[0]->_direction.y, directional_lights[0]->_direction.z);
           glUniform3f(glGetUniformLocation(this->_shader._program, "dirLight.ambient"), directional_lights[0]->_ambient.x, directional_lights[0]->_ambient.y, directional_lights[0]->_ambient.z);
           glUniform3f(glGetUniformLocation(this->_shader._program, "dirLight.diffuse"), directional_lights[0]->_diffuse.x, directional_lights[0]->_diffuse.y, directional_lights[0]->_diffuse.z);
