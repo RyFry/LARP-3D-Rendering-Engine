1649dcf36bd5c6eb33020cedb77af610dcda8c2b
Zin20 <jimmyne@gmail.com>
2016-04-28 11:18:52 -0500

    I fucked up master....

---
+++ src/SoundManager.cpp
@@ -0,0 +1,16 @@
+#include "SoundManager.hpp"
+
+SoundManager::SoundManager()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	srand(time(NULL));
+	Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096);
+
+
+}
+
+SoundManager::~SoundManager()
+{
+
+}
---
+++ src/SoundManager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+#include "LarpPrerequisites.hpp"
+#include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
+	
+class SoundManager
+	{
+		SoundManager();
+		~SoundManager();
+	};
+
