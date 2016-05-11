1531a8484daa95e9d9d6111ceaf33dc45ad1708f
Ryan Lorey <ryanlorey@outlook.com>
2016-05-10 21:20:12 -0500

    Maybe fixing physics

--- src/Physics/PhysicsObject.cpp
+++ src/Physics/PhysicsObject.cpp
@@ -6,11 +6,12 @@ PhysicsObject<BulletShape>::PhysicsObject(glm::quat rotation, glm::vec3 position
                                           Larp::NodePtr user_pointer)
 {
     btTransform transform;
+    transform.setIdentity();
     transform.setOrigin(btVector3(position.x, position.y, position.z));
     transform.setRotation(btQuaternion(rotation.x, rotation.y, rotation.z, rotation.w));
 
     btScalar bt_mass(mass);
-    btVector3 inertia(local_inertia.x, local_inertia.y, local_inertia.z);
+    btVector3 inertia;
 
     btCollisionShape* shape = new BulletShape(btVector3(user_pointer->get_scaled_width(),
                                                         user_pointer->get_scaled_height(),
@@ -21,9 +22,13 @@ PhysicsObject<BulletShape>::PhysicsObject(glm::quat rotation, glm::vec3 position
 
     btRigidBody::btRigidBodyConstructionInfo rigid_body_info(bt_mass, motion_state,
                                                              shape, inertia);
+    rigid_body_info.m_linearDamping = .2f;
+    rigid_body_info.m_angularDamping = .2f;
     this->_rigid_body = new btRigidBody(rigid_body_info);
     this->_rigid_body->setRestitution(restitution);
     this->_rigid_body->setUserPointer(user_pointer);
+    this->_rigid_body->setFriction(.5f);
+    this->_rigid_body->setRollingFriction(.5f);
 }
 
 template <typename BulletShape>
