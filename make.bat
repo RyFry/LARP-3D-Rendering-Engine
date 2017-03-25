f56207ab9cfa1d352b34eca409fa4df8bc10337c
RyFry <ryanlorey@outlook.com>
2017-03-24 20:58:25 -0700

    Beginning of D3D code

---
+++ make.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+set SOURCES=src\D3DRefactor\main.cpp
+set INCLUDES=/I"%DXSDK_DIR%Include\"
+set OPTIONS=/Zi /Foobj\game.obj /Fdobj\game.pdb
+set LINK=user32.lib d3d11.lib d3dx11.lib d3dx10.lib
+set OUTPUT=bin\game.exe
+set CXX=cl.exe
+set LINKEROPTIONS=/link /OUT:out/game.exe /PDB:out/game.pdb
+
+echo. 
+echo ========= COMMAND =========
+echo %CXX% %OPTIONS% %SOURCES% %LINK% /OUT:%OUTPUT% %LINKEROPTIONS%
+echo =========== END ===========
+echo.
+
+md out
+md obj
+%CXX% %OPTIONS% %SOURCES% %LINK% /OUT:%OUTPUT% %LINKEROPTIONS%
