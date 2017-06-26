8ae4784afba748720d0ecebe4a4575298c5d5b61
RyFry <ryanlorey@outlook.com>
2017-06-25 17:43:21 -0700

    Update make.bat

--- make.bat
+++ make.bat
@@ -2,21 +2,29 @@
 setlocal
 
 set SOURCES=src\D3DRefactor\main.cpp
-set INCLUDES=/I"%DXSDK_DIR%Include\" /I\include
-set OPTIONS=/Zi /Foobj\game.obj /Fdobj\game.pdb
+set INCLUDES=/I"%DXSDK_DIR%Include\" /I\include /I\src
+set OPTIONS=/Zi /Fdobj\game.pdb /Feout\game.exe
 set LINK=user32.lib d3d11.lib d3dx11.lib D3DCompiler.lib dxerr.lib dxgi.lib dxguid.lib
 set OUTPUT=out\game
 set CXX=cl.exe
-set LINKEROPTIONS=/link /OUT:%OUTPUT%.exe /PDB:%OUTPUT%.pdb
+set LINKEROPTIONS=
 
-echo. 
-echo ========= COMMAND =========
-echo %CXX% %OPTIONS% %SOURCES% %INCLUDES% %LINK% %LINKEROPTIONS%
-echo =========== END ===========
-echo.
+set SHOULDBUILD=F
+if not exist %OUTPUT%.exe set SHOULDBUILD=T
+if "%1" == "" set SHOULDBUILD=T
+if "%SHOULDBUILD%" == "T" (
+   echo. 
+   echo ========= COMMAND =========
+   echo %CXX% %OPTIONS% %SOURCES% %INCLUDES% %LINK% %LINKEROPTIONS%
+   echo =========== END ===========
+   echo.
 
-md out
-md obj
-%CXX% %OPTIONS% %SOURCES% %INCLUDES% %LINK% %LINKEROPTIONS%
+   md out
+   md obj
+   %CXX% %OPTIONS% %SOURCES% %INCLUDES% %LINK% %LINKEROPTIONS%
+)
 
+if "%1" == "run" (
+   %OUTPUT%.exe
+)
 
